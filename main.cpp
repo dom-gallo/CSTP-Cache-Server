cc0c638e79f1a21df8312225d3117cfa016813e1
Dominic Gallo <dgallo@dominics-mbp.myfiosgateway.com>
2022-04-30 16:52:23 -0400

    refactoring some more stuff

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 /*
+ * CSTP_server:
+ * Built on top of a boilerplate echo server written in C.
+ * usage:  <port>
+ *
  * echoserver.c - A simple connection-based echo server
- * usage: echoserver <port>
+ *
  */
 
 #include <stdio.h>
@@ -8,45 +12,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netdb.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <iostream>
-//#include "RequestHandler.h"
 #include "RequestHandler.cpp"
 #define BUFSIZE 1024
 
 #if 0
-/*
- * Structs exported from netinet/in.h (for easy reference)
- */
-
-/* Internet address */
-struct in_addr {
-  unsigned int s_addr;
-};
-
-/* Internet style socket address */
-struct sockaddr_in  {
-  unsigned short int sin_family; /* Address family */
-  unsigned short int sin_port;   /* Port number */
-  struct in_addr sin_addr;	 /* IP address */
-  unsigned char sin_zero[...];   /* Pad to size of 'struct sockaddr' */
-};
-
-/*
- * Struct exported from netdb.h
- */
-
-/* Domain name service (DNS) host entry */
-struct hostent {
-  char    *h_name;        /* official name of host */
-  char    **h_aliases;    /* alias list */
-  int     h_addrtype;     /* host address type */
-  int     h_length;       /* length of address */
-  char    **h_addr_list;  /* list of addresses */
-}
 #endif
 int getListenFD();
 void buildServerAddress(struct sockaddr_in &server_in, const int portno);
